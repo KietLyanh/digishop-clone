3f2c6e4563c2d91a10171877ebe6f5a7b475ec3c
tuyen <ngotrungtuyen080703@gmail.com>
2023-12-05 21:47:27 +0700

    update hotdeal

--- my_app/src/pages/home/Hotdeal/index.js
+++ my_app/src/pages/home/Hotdeal/index.js
@@ -1,1 +1,1 @@
-export {default} from "./hotdeal"
+export {default} from "./Hotdeal"
--- my_app/src/pages/home/index.js
+++ my_app/src/pages/home/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './home.scss';
 import LinkPage from "./LinkPage";
+import Hotdeal from "./Hotdeal";
 
 function home()
 {
     return(
         <div className="body-content">
             <LinkPage/>
+            <Hotdeal/>
         </div>
     );
 }
